7cfd21f2ef6d0d19e760dae30703a4680d030757
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-08-02 18:17:20 +0400

    feat :  add types.ts file

---
+++ app/types/types.ts
@@ -0,0 +1,40 @@
+// users information
+export interface User {
+  id: number;
+  userName: string;
+  email: string;
+  avatar: string;
+  status: UserStatus;
+}
+// users status
+export type UserStatus = 'online' | 'offline' | 'away' | 'busy';
+// Authentification  interface
+export interface AuthState {
+    user: User | null;       
+    isAuthenticated: boolean; 
+    isLoading: boolean;     
+    error: string | null;    
+}; 
+
+//ogin/register  froms
+export interface LoginCredentials {
+    username: string;   
+    password: string;  
+}
+export interface RegisterCredentials {
+    username: string;
+    email: string;
+    password: string;      
+    confirmPassword: string;  
+}
+
+//AuthActions interface 
+export interface AuthActions {
+    login: (credentials: LoginCredentials) => Promise<void>;
+    register: (credentials: RegisterCredentials) => Promise<void>; 
+    logout: () => void;  
+    setLoading: (loading: boolean) => void;  
+    setUser: (user: User | null) => void;   
+    checkAuth: () => void;
+    updateProfile: (updates: Partial<User>) => void;  
+}
